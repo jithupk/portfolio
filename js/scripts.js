aea36571f400d9e5704bfdc7684905ea5b6316a1
Jithu PK <jithupk96@gmail.com>
2025-12-01 00:46:00 +0530

    new about section

--- js/scripts.js
+++ js/scripts.js
@@ -157,4 +157,17 @@ window.addEventListener("scroll", handleHeaderScroll);
 //     });
 //     ticking = true;
 //   }
-// });
+// }); 
+
+
+$(document).ready(function () {
+  $(".btn-primary, .btn-secondary, .btn-dark").each(function () {
+    const text = $(this).text().trim();
+    $(this).html(
+      `<div class="button_text-wrapper"><div class="button_text">${text} </div></div>
+      <svg xmlns="http://www.w3.org/2000/svg" width="100%" viewBox="0 0 14 12" fill="none" class="button_square-arrow-icon is-relative" style="transform: translate3d(0em, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;"><path d="M13.0306 6.53025L8.53063 11.0302C8.38973 11.1711 8.19863 11.2503 7.99938 11.2503C7.80012 11.2503 7.60902 11.1711 7.46813 11.0302C7.32723 10.8894 7.24807 10.6983 7.24807 10.499C7.24807 10.2997 7.32723 10.1086 7.46813 9.96775L10.6875 6.74962H1.5C1.30109 6.74962 1.11032 6.6706 0.96967 6.52995C0.829018 6.3893 0.75 6.19853 0.75 5.99962C0.75 5.80071 0.829018 5.60994 0.96967 5.46929C1.11032 5.32864 1.30109 5.24962 1.5 5.24962H10.6875L7.46937 2.02962C7.32848 1.88873 7.24932 1.69763 7.24932 1.49837C7.24932 1.29911 7.32848 1.10802 7.46937 0.967121C7.61027 0.826225 7.80137 0.74707 8.00062 0.74707C8.19988 0.74707 8.39098 0.826225 8.53187 0.967121L13.0319 5.46712C13.1018 5.53689 13.1573 5.61979 13.1951 5.71106C13.2329 5.80232 13.2523 5.90016 13.2522 5.99894C13.252 6.09773 13.2324 6.19552 13.1944 6.28669C13.1564 6.37787 13.1007 6.46064 13.0306 6.53025Z" fill="currentColor"></path></svg>
+      `
+    );
+  });
+});
+
